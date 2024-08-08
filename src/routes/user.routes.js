55a8809c872e0c3fde400cf64f78839f1361ac68
Prins <prins.implies@gmail.com>
2024-08-08 14:17:26 +0530

    change the code structure in route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,17 +5,11 @@ import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(
-    upload.fields([
-        {
-            name: "profileImage",
-            maxCount: 1
-        }
-    ]),
+router.post("/register", 
+    upload.fields([{ name: "profileImage", maxCount: 1 }]), 
     registerUser
-)
-router.route("/login").post(loginUser)
-router.route("/logout").post(verifyJWT, logoutUser)
-
+);
+router.post("/login", loginUser)
+router.post("/logout", verifyJWT, logoutUser)
 
 export default router;
